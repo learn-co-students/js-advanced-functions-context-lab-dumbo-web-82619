cc6f48c590956f469926cd348b76d714d512f603
dangrammer <danieljromans@gmail.com>
2020-01-05 18:06:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const createEmployeeRecord = (arr) => {
   return {
-      firstName: arr[0],
-      familyName: arr[1],
-      title: arr[2],
-      payPerHour: arr[3],
-      timeInEvents: [],
-      timeOutEvents: []
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: []
   }    
 }
 
