24da603e1960e6c31b79e0d850055081849ace30
dangrammer <danieljromans@gmail.com>
2020-01-05 18:04:08 -0500

    Done.

--- index.js
+++ index.js
@@ -43,21 +43,20 @@ function wagesEarnedOnDate(date) {
   return hoursWorkedOnDate.call(this, date) * this.payPerHour   
 }
 
-/*
-We're giving you this function. Take a look at it, you might see some usage
-that's new and different. That's because we're avoiding a well-known, but
-sneaky bug that we'll cover in the next few lessons!
+function calculatePayroll(arr) {
+  return arr.reduce((total, record) => total += allWagesFor.call(record), 0)
+}
 
-As a result, the lessons for this function will pass *and* it will be available
-for you to use if you need it!
-*/
+function findEmployeeByFirstName(arr, name) {
+    return arr.find(record => record.firstName === name)
+}
 
 let allWagesFor = function () {
-  let eligibleDates = this.timeInEvents.map(function (e) {
-    return e.date
-  })
-  let payable = eligibleDates.reduce(function (memo, d) {
-    return memo + wagesEarnedOnDate.call(this, d)
-  }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-  return payable
-}
+    let eligibleDates = this.timeInEvents.map(function (e) {
+      return e.date
+    })
+    let payable = eligibleDates.reduce(function (memo, d) {
+      return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0)
+    return payable
+  }
